c4a762bfb1112c75aeba73165e3349b46d411d4c
rodrigues882013 <rodrigues882007@gmail.com>
2017-06-27 01:38:11 -0300

    -- refactoring layouts -- adding seach activity -- removing some boilerplates

---
+++ app/src/main/java/com/example/felipe/desafiomobfiq/SearchActivity.java
@@ -0,0 +1,13 @@
+package com.example.felipe.desafiomobfiq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+}
